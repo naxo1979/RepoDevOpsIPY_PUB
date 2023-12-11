ba1d91c69bae1c57d31598c64b6b483814fa2866
naxo1979 <152732456+naxo1979@users.noreply.github.com>
2023-12-11 21:13:32 +0100

    Update util_test.py

--- test/unit/util_test.py
+++ test/unit/util_test.py
@@ -23,7 +23,7 @@ class TestUtil(unittest.TestCase):
         self.assertRaises(TypeError, util.convert_to_number, None)
         self.assertRaises(TypeError, util.convert_to_number, object())
 
-    def test_convert_to_number_correct_param(self):
+    def test_convert_to_number_correct_param1(self):
         self.assertEqual(4, util.convert_to_number("4"))
         self.assertEqual(0, util.convert_to_number("0"))
         self.assertEqual(0, util.convert_to_number("-0"))
@@ -33,7 +33,7 @@ class TestUtil(unittest.TestCase):
         self.assertAlmostEqual(0.0, util.convert_to_number("-0.0"), delta=0.0000001)
         self.assertAlmostEqual(-1.0, util.convert_to_number("-1.0"), delta=0.0000001)
 
-    def test_convert_to_number_invalid_type(self):
+    def test_convert_to_number_invalid_type1(self):
         self.assertRaises(TypeError, util.convert_to_number, "")
         self.assertRaises(TypeError, util.convert_to_number, "3.h")
         self.assertRaises(TypeError, util.convert_to_number, "s")
